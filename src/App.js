744878724b8c3d57d3cf0c94f2bea17e7d78cdaf
TaimaDarwish02 <taima.darwish@hotmail.com>
2024-08-27 14:18:57 +0300

    JSON enhancement

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import Header from "./components/Header"
 import Tasks from './components/Tasks'
-import { useState } from "react"
+import { useState,useEffect } from "react"
+import AddTask from './components/AddTask'
+import { BsDatabaseAdd } from 'react-icons/bs'
 
 function App() {
-  const[tasks,setTasks]=useState([
-    {
-        id:1,
-        text:'Doctors Appointment',
-        day:'Feb 5th at 2:30pm',
-        reminder:true,
-    },
-    {
-        id:2,
-        text:'Meeting at school',
-        day:'Feb 6th at 1:30pm',
-        reminder:true,
-    },
-    {
-        id:3,
-        text:'Food Shopping',
-        day:'Feb 5th at 2:30pm',
-        reminder:false,
-    },
-])
+  const[showAddTask,setShowAddTask]=useState(false)
+  const[tasks,setTasks]=useState([])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:3000/tasks');
+    const data = await res.json();
+    return data;
+  };
+
+  // Fetch tasks on component mount
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+//add tasks 
+const addTask=(task)=>{
+const id=Math.floor(Math.random()*10)+1//random id number 
+const newTask={id, ...task}
+setTasks([...tasks,newTask])
+}
 //delete task 
 const deleteTask=(id)=>{
   setTasks(tasks.filter((task)=>task.id !==id))
@@ -39,7 +44,12 @@ const toggleReminder=(id)=>{
 }
   return (
     <div className="container">
-      <Header />
+      <Header 
+      onAdd={()=>setShowAddTask
+      (!showAddTask)}
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ?( < Tasks tasks={tasks}
        onDelete={deleteTask} onToggle={toggleReminder}/>)
       :(
